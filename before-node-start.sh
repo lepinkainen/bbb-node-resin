fdc612308bc18570eb0e4721e0d8f1d166c39999
Riku Lindblad <riku.lindblad@gmail.com>
2016-03-24 10:08:05 +0200

    Autostart mosquitto

--- before-node-start.sh
+++ before-node-start.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 #use this script to launch anything you need before your node.js project launches.
-echo "Starting adc.js"
+echo "Starting Home automation server"
 
+echo "Starting 1-wire"
 mkdir -p /mnt/1wire
 /etc/init.d/owserver start
 owfs
 
+echo "Starting mosquitto"
+/etc/init.d/mosquitto start
