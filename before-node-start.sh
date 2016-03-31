1d829c02dfca52cd0254cd11c6c25d85134502e7
Riku Lindblad <riku.lindblad@gmail.com>
2016-03-31 23:49:30 +0300

    Don't keep supervisor in the foreground

--- before-node-start.sh
+++ before-node-start.sh
@@ -5,4 +5,4 @@ export PASSWD=${PASSWD:=root}
 echo "root:$PASSWD" | chpasswd
 
 echo "Starting supervisor"
-supervisord -n -c /usr/local/etc/supervisord.conf
+supervisord -c /usr/local/etc/supervisord.conf
