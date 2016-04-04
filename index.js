f41d33c8be7a58c0982310e773d45448cbc28a8c
Riku Lindblad <riku.lindblad@gmail.com>
2016-04-04 16:01:01 +0300

    Add socket.io for client-server connections

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 var express = require('express');
+var app = express()
+var ioserver = require('http').Server(app);
+var io = require('socket.io')(ioserver);
+
 //var knockout = require("knockout");
 //var mygpio = require("./gpio");
 //mygpio.init();
@@ -10,12 +14,10 @@ var readADC = function() {
     voltage = Math.random();
 };
 
-// Webserver
-var app = express()
 
 //tell express to serve static files from html
 app.use(express.static(__dirname+"/html"));
-
+app.use('/scripts', express.static(__dirname+"/scripts"));
 
 // Get states for all configured GPIO pins
 app.get("/api/gpio", function(req, res) {
@@ -34,6 +36,13 @@ app.put("/api/gpio/:gpio", function(req, res) {
     console.log("GPIO set: "+req.params.gpio+": " + JSON.stringify(req.body, null, 4));
     res.send(200);
     res.end();
+});
+
+io.on('connection', function (socket) {
+    socket.emit('news', { hello: 'world' });
+    socket.on('my other event', function (data) {
+        console.log(data);
+    });
 });
 
 var server = app.listen(80, function () {
